abe5ee6faac9d7c358a09821c4d3736d4df83e65
ruiliang.mrl <ruiliang.mrl@alibaba-inc.com>
2018-04-23 01:16:10 +0800

    travelgift.

--- travelgift-booter/src/main/java/net/uchoice/travelgift/Bootstrap.java
+++ travelgift-booter/src/main/java/net/uchoice/travelgift/Bootstrap.java
@@ -1,4 +1,4 @@
-package net.uchoice.vote;
+package net.uchoice.travelgift;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 @SpringBootApplication
-@MapperScan("net.uchoice.dao")
+@MapperScan("net.uchoice.travelgift.vote.dao")
 @EnableAutoConfiguration(exclude = { JpaRepositoriesAutoConfiguration.class// 禁止springboot自动加载持久化bean
 })
 public class Bootstrap {
